a938b8a888ec299ffe2983ca0627fe6daad3411c
jazielg <jazielgj1995@gmail.com>
2020-06-25 21:17:52 -0300

    funcionalidades avancadas

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
 const parseStringAsArray = require("../utils/parseStringAsArray");
-const { update } = require("../models/Dev");
+const { findConnections, sendMessage } = require("../websocket");
 
 // Index, show, store, update, destroy
 
@@ -40,6 +40,14 @@ module.exports = {
         techs: techsArray,
         location,
       });
+
+      // Filtrar as conexões que estão há no máximo 10km de distância
+      // e que o novo dev tenha pelo menos uma das tecnologias filtradas
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray
+      );
+      sendMessage(sendSocketMessageTo, "new-dev", dev);
     }
 
     return response.json(dev);
